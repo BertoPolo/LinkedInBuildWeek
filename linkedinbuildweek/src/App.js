907b1e2bb69c82fc929314fac469cf538f323a33
BertoPolo <bertopolo91@gmail.com>
2022-03-28 17:11:54 +0200

    structuring a little bit

--- linkedinbuildweek/src/App.js
+++ linkedinbuildweek/src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "./App.css"
 import SideBar from "./Components/SideBar"
+import { Container, Row } from "react-bootstrap"
 // import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MyFooter from "./Components/MyFooter"
 import MyNavbar from "./Components/MyNavbar"
@@ -12,12 +13,14 @@ function App() {
     <>
       {/* <BrowserRouter> */}
       <MyNavbar />
-      {/* <Routes> */}
-      <MyProfile />
-      {/* </Routes> */}
-      {/* <Route path="" element={}> */}
-      <SideBar />
-      {/* </BrowserRouter> */}
+      <Container>
+        {/* <Routes> */}
+        <MyProfile />
+        {/* </Routes> */}
+        {/* <Route path="" element={}> */}
+        <SideBar />
+        {/* </BrowserRouter> */}
+      </Container>
       <MyFooter />
     </>
   )
