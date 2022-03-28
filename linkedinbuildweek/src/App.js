db80ba920084138cfe6e6158d031330d2d46e4f5
giorgio <giorgiolink94@hotmail.it>
2022-03-28 12:04:08 +0100

    Update

--- linkedinbuildweek/src/App.js
+++ linkedinbuildweek/src/App.js
@@ -1,7 +1,7 @@
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
-import SideBar from "./Components/SideBar"
+import SideBar from "./components/SideBar"
 // import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MyFooter from "./components/MyFooter"
 import MyNavbar from "./components/MyNavbar"
