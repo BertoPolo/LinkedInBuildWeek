2a169879cfa117d6fdc72cad649cf32f4f6a5486
giorgio <giorgiolink94@hotmail.it>
2022-03-28 10:39:47 +0100

    Update

--- linkedinbuildweek/src/App.js
+++ linkedinbuildweek/src/App.js
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import SideBar from "./Components/SideBar"
 
 function App() {
-  return <></>
+  return <>
+  <SideBar/>
+    </>
 }
 
 export default App
