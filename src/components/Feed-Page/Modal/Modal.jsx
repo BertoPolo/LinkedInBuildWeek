9c83be79f3954efc3c543e0446d2ccbe9446b72e
giorgio <giorgiolink94@hotmail.it>
2022-03-30 15:40:07 +0100

    Update

--- src/components/Feed-Page/Modal/Modal.jsx
+++ src/components/Feed-Page/Modal/Modal.jsx
@@ -1,25 +1,32 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Container } from "react-bootstrap"
-import "./Modal.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import "./Modal.css";
 
 const ModalPost = ({ PostingFunction, setPost, handleClose }) => {
   return (
     <>
-      <Container id="box" className="mainBox">
+      <Container id="box44" className="mainBox44">
         <div className="row">
           <div>
-            <h3 className="createPost">Create a post</h3>
+            <h3 className="createPost44">Create a post</h3>
           </div>
-          <hr id="break" />
+          <hr id="break44" />
 
-          <div id="mainContainer" className="row">
-            <div className="firstContainer col-2">
-              <img id="profilePic2" src="" alt="" />
+          <div id="mainContainer44" className="row">
+            <div className="firstContainer44 col-2">
+              <img id="profilePic244" src="" alt="" />
             </div>
-            <div className="firstContainer col-10">
-              <span id="nameFont">NAME</span>
-              <button className="buttonPrivacy">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+            <div className="firstContainer44 col-10">
+              <span id="nameFont44">NAME</span>
+              <button className="buttonPrivacy44">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-globe"
+                  viewBox="0 0 16 16"
+                >
                   <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
                 </svg>
                 Anyone
@@ -27,12 +34,16 @@ const ModalPost = ({ PostingFunction, setPost, handleClose }) => {
             </div>
           </div>
 
-          <div className="commentSection">
-            <textarea type="textarea" name="" id="inputBox" onChange={(e) => setPost(e.target.value)} />
-
-            <button className="addHas">Add Hashtag</button>
+          <div className="commentSection44">
+            <textarea
+              type="textarea"
+              name=""
+              id="inputBox44"
+              onChange={(e) => setPost(e.target.value)}
+            />
+            <button className="addHas44">Add Hashtag</button>
           </div>
-          <div className="bottomContainer">
+          <div className="bottomContainer44">
             <button className="buttonSrc">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -127,8 +138,8 @@ const ModalPost = ({ PostingFunction, setPost, handleClose }) => {
                 <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
               </svg>
             </button>
-            <div class="vl"></div>
-            <button className="buttonPrivacy2">
+            <div class="vl44"></div>
+            <button className="buttonPrivacy244">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -144,7 +155,11 @@ const ModalPost = ({ PostingFunction, setPost, handleClose }) => {
               Anyone
             </button>
             <div>
-              <button onClick={PostingFunction()} handleClose={handleClose()} className="lastButton">
+              <button
+                onClick={PostingFunction()}
+                handleClose={handleClose()}
+                className="lastButton44"
+              >
                 Post
               </button>
             </div>
@@ -152,7 +167,7 @@ const ModalPost = ({ PostingFunction, setPost, handleClose }) => {
         </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default ModalPost
+export default ModalPost;
