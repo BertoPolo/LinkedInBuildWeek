7a074d2b6b62c80b14aacb9290fe107c9db82587
giorgio <giorgiolink94@hotmail.it>
2022-03-30 14:19:56 +0100

    Update Components

--- src/components/Feed-Page/Feed-Page.jsx
+++ src/components/Feed-Page/Feed-Page.jsx
@@ -1,9 +1,14 @@
+import Modal from "./Modal/Modal";
 import FeedNavigation from "./Feed-Navigation/Feed-SearchBar";
+import FeedPost from "./Feed-Posts/FeedPosts";
 
 const FeedPage = () => {
   return (
     <>
       <FeedNavigation />
+
+      <FeedPost />
+      <Modal />
     </>
   );
 };
