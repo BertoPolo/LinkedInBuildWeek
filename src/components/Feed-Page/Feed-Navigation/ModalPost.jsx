598e140faca5d6479c53c81029cd146888ab7d27
giorgio <giorgiolink94@hotmail.it>
2022-04-29 08:18:43 +0100

    Fix

--- src/components/Feed-Page/Feed-Navigation/ModalPost.jsx
+++ src/components/Feed-Page/Feed-Navigation/ModalPost.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-lone-blocks */
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Button, Modal } from "react-bootstrap"
-import "./ModalPost.css"
-import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Modal } from "react-bootstrap";
+import "./ModalPost.css";
+import { useState } from "react";
 // import { useNavigate } from "react-router-dom"
 
 const ModalPost = ({ show, handleClose, postID }) => {
   const [post, setPost] = useState({
     text: "",
-  })
+  });
   // const navigate = useNavigate()
 
   const asyncFunction = async (methodToDo) => {
     try {
       const response = await fetch(
-        postID ? "https://striveschool-api.herokuapp.com/api/posts/" + postID : "https://striveschool-api.herokuapp.com/api/posts/",
+        postID
+          ? "https://striveschool-api.herokuapp.com/api/posts/" + postID
+          : "https://striveschool-api.herokuapp.com/api/posts/",
         {
           method: methodToDo,
           body: JSON.stringify(post),
@@ -24,17 +26,17 @@ const ModalPost = ({ show, handleClose, postID }) => {
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQxNmU3ZWQzMzk4NDAwMTVjODgzYjYiLCJpYXQiOjE2NDg0NTUyOTgsImV4cCI6MTY0OTY2NDg5OH0.VLQs1aPcryvd-GdlD9l8Fl80QZPNQHjrbWcVQpEBvCA",
           },
         }
-      )
+      );
 
       if (response.ok) {
-        alert("POSTED!!")
+        alert("POSTED!!");
       } else {
-        alert("we have a problem")
+        alert("we have a problem");
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   return (
     <>
@@ -43,15 +45,27 @@ const ModalPost = ({ show, handleClose, postID }) => {
           <Modal.Title>Create a post</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <textarea type="textarea" name="" id="inputBox44" onChange={(e) => setPost({ text: e.target.value })} value={post.text} />
+          <textarea
+            type="textarea"
+            name=""
+            id="inputBox44"
+            onChange={(e) => setPost({ text: e.target.value })}
+            value={post.text}
+          />
         </Modal.Body>
         <Modal.Footer>
           {postID ? (
             <>
-              <Button variant="danger" onClick={(handleClose, () => asyncFunction("DELETE"))}>
+              <Button
+                variant="danger"
+                onClick={(handleClose, () => asyncFunction("DELETE"))}
+              >
                 Delete
               </Button>
-              <Button variant="success" onClick={(handleClose, () => asyncFunction("PUT"))}>
+              <Button
+                variant="success"
+                onClick={(handleClose, () => asyncFunction("PUT"))}
+              >
                 Update
               </Button>
               <Button variant="secondary" onClick={handleClose}>
@@ -63,7 +77,10 @@ const ModalPost = ({ show, handleClose, postID }) => {
               <Button variant="secondary" onClick={handleClose}>
                 Close
               </Button>
-              <Button variant="primary" onClick={(handleClose, () => asyncFunction("POST"))}>
+              <Button
+                variant="primary"
+                onClick={(handleClose, () => asyncFunction("POST"))}
+              >
                 Post!
               </Button>
             </>
@@ -71,7 +88,7 @@ const ModalPost = ({ show, handleClose, postID }) => {
         </Modal.Footer>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default ModalPost
+export default ModalPost;
